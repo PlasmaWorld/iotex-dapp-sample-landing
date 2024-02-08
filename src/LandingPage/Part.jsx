1b9ebbe5694a3fc5331c4854287ae6da37d31bfa
shahriarmehedi <67407302+shahriarmehedi@users.noreply.github.com>
2024-02-08 23:32:50 +0600

    init

---
+++ src/LandingPage/Part.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Part() {
+    return (
+        <div>Part</div>
+    )
+}
+
+export default Part
---
+++ src/pages/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { createStyles, Container, Text, Button, Group, useMantineTheme } from '@mantine/core';
+import MainLayout from '@/components/Layout';
+import { useTranslation } from 'react-i18next';
+import { useStore } from '../store';
+import { LanguageSwitch } from '@/components/LanguageSwitch';
+import { helper } from '@/lib/helper';
+import { WagmiProvider } from '@/components/WagmiProvider';
+
+import Hero from '../LandingPage/Hero';
+import FirstStep from '../LandingPage/FirstStep';
+import Meet from '../LandingPage/Meet';
+import Stats from '../LandingPage/Stats';
+import Part from '../LandingPage/Part';
+import Roadmap from '../LandingPage/Roadmap';
+import FAQ from '../LandingPage/FAQ';
+
+export default function HeroTitle() {
+
+
+  return (
+    <MainLayout>
+      <Hero />
+      <FirstStep />
+      <Meet />
+      <Stats />
+      <Part />
+      <Roadmap />
+      <FAQ />
+    </MainLayout>
+  );
+}
